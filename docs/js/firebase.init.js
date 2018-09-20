abd2659c055c77dc66834c78c42c212af5d70971
trossides <theorossides@gmail.com>
2018-09-20 19:58:38 +0930

    Add docs file for git page

---
+++ docs/js/firebase.init.js
@@ -0,0 +1,45 @@
+angular.module('firebaseConfig', ['firebase'])
+
+.run(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDgC7WbYIVLnAyCql2fXOhwsCERl-HcdqY",
+    authDomain: "authfirebase-40b1e.firebaseapp.com",
+    databaseURL: "https://authfirebase-40b1e.firebaseio.com",
+    storageBucket: "authfirebase-40b1e.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  /*const messaging = firebase.messaging();
+  messaging.requestPermission().then(function() {
+    console.log('Notification permission granted.');
+  })
+  .catch(function(err) {
+    console.log('Unable to get permission to notify.', err);
+  });*/
+
+})
+//
+/*
+
+.service("TodoExample", ["$firebaseArray", function($firebaseArray){
+    var ref = firebase.database().ref().child("todos");
+    var items = $firebaseArray(ref);
+    var todos = {
+        items: items,
+        addItem: function(title){
+            items.$add({
+                title: title,
+                finished: false
+            })
+        },
+        setFinished: function(item, newV){
+            item.finished = newV;
+            items.$save(item);
+        }
+    }
+    return todos;
+}])
+
+*/
