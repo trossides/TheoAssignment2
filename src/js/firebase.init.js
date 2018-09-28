3bfafb57601e83784681d5b863535c91fa266729
trossides <theorossides@gmail.com>
2018-09-28 15:40:35 +0930

    ERROR FIX - FINAL

--- src/js/firebase.init.js
+++ src/js/firebase.init.js
@@ -8,8 +8,22 @@ angular.module('firebaseConfig', ['firebase'])
     authDomain: "authfirebase-40b1e.firebaseapp.com",
     databaseURL: "https://authfirebase-40b1e.firebaseio.com",
     storageBucket: "authfirebase-40b1e.appspot.com",
+    messagingSenderId: "848381304196"
   };
   firebase.initializeApp(config);
+
+  const messaging = firebase.messaging();
+
+  messaging.requestPermission().then(function() {
+    console.log('Notification permission granted.');
+    // TODO(developer): Retrieve an Instance ID token for use with FCM.
+    // ...
+  }).catch(function(err) {
+    console.log('Unable to get permission to notify.', err);
+  });
+  
+
+  //messaging.usePublicVapidKey('AIzaSyCy6Pj5L2JHERiPObGdp4-2dsn2sR3St-Y');
 
   /*const messaging = firebase.messaging();
   messaging.requestPermission().then(function() {
